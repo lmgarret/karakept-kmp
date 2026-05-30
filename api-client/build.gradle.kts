04c82a39f4dc179113a6f72c214ec82a8527f9af
LM. Garret <9049104+lmgarret@users.noreply.github.com>
2026-05-30 12:59:21 +0000

    build: raise Android JVM target to 11
    Several upgraded dependencies (Compose Multiplatform 1.11, Coil 3.4,
    coroutines 1.11) ship inline functions compiled for JVM 11, which cannot be
    inlined into the Android target while it compiles at JVM 1.8. Raise jvmTarget
    and compileOptions to 11 in composeApp and api-client. minSdk 24 and AGP 8.13
    fully support JVM 11 bytecode.

--- api-client/build.gradle.kts
+++ api-client/build.gradle.kts
@@ -12,7 +12,7 @@ val generatedSourcesDir = layout.buildDirectory.dir("generated/openapi")
 kotlin {
     androidTarget {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
     
@@ -44,8 +44,8 @@ android {
     }
     
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
